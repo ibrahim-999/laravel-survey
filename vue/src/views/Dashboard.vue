bbdf0b6d87fcf2f54e940a974e767d2fef6d56a7
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2022-02-06 10:29:16 +0200

    [DEV]: composable-component

--- vue/src/views/Dashboard.vue
+++ vue/src/views/Dashboard.vue
@@ -1,28 +1,14 @@
 <template>
-  <header class="bg-white shadow">
-    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold text-gray-900">
-        Dashboard
-      </h1>
-    </div>
-  </header>
-  <main>
-    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-      <!-- Replace with your content -->
-      <div class="px-4 py-6 sm:px-0">
-        Content goes
-      </div>
-      <!-- /End replace -->
-    </div>
-  </main>
+  <PageComponent
+    title = "Dashboard">
+    content goes here
+  </PageComponent>
 </template>
 
-<script>
-    export default {
-        name: "Dashboard"
-    }
+<script setup>
+import PageComponent from "../components/PageComponent.vue";
+
 </script>
-a
 <style scoped>
 
 </style>
