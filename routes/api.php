c7cceec168ccbc58060b067331a3de2f4e5bfcb6
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2022-02-06 13:36:41 +0200

    [DEV]: bc-logout

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::post('/register', [AuthController::class,'register']);
 Route::post('/login', [AuthController::class,'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class,'logout']);
+
+});
 
 
