a799475e4528579d6875b9c1fc577f6e7589a08c
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2022-02-07 11:58:16 +0200

    [DEV]: crud-api-survey

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SurveyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,6 +24,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::resource('/survey', SurveyController::class);
     Route::post('/logout', [AuthController::class,'logout']);
 
 });
